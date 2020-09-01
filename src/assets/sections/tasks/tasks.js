12bafd98751d5d95f495b7ec09d5b20bd400f7a1
Скобелев Денис <generaldennis88@gmail.com>
2020-09-01 10:02:48 +0300

    some additions and fixes

--- src/assets/sections/tasks/tasks.js
+++ src/assets/sections/tasks/tasks.js
@@ -1,8 +1,9 @@
 import './tasks.sass'
 
 $(document).ready(function() {
-  const $item = $('.aside-filter__item')
-  const $check = $('.filter-content__item')
+  const $item = $('.aside-filter__item');
+  const $check = $('.filter-content__item');
+  const $clearFilter = $('.js-filter-clear');
 
   if ($item) {
     $item.on('click', (e) => {
@@ -16,7 +17,13 @@ $(document).ready(function() {
   }
   if ($check) {
     $check.on('click', (e) => {
-      $(e.currentTarget).toggleClass('radio--active')
+      $(e.currentTarget).toggleClass('radio--active');
+      $clearFilter.removeClass('aside-filter__button--hide');
     })
   }
+  if ($clearFilter)
+    $clearFilter.on('click', () => {
+      $check.removeClass('radio--active');
+      $clearFilter.addClass('aside-filter__button--hide');
+    })
 })
--- src/assets/sections/tutorial/tutorial.js
+++ src/assets/sections/tutorial/tutorial.js
@@ -1,7 +1,66 @@
 $(document).ready(() => {
-  const $item = $('.aside__item')
-  const $btn1 = $('#play-btn1')
-  const video1 = $('#video1').get(0)
+  const $item = $('.aside__item');
+  const $play1 = $('#play-btn1');
+  const $pause1 = $('#pause-btn1');
+  const video1 = $('#video1').get(0);
+
+  const $play2 = $('#play-btn2');
+  const $pause2 = $('#pause-btn2');
+  const video2 = $('#video2').get(0);
+
+  const $play3 = $('#play-btn3');
+  const $pause3 = $('#pause-btn3');
+  const video3 = $('#video3').get(0);
+
+  const $play4 = $('#play-btn4');
+  const $pause4 = $('#pause-btn4');
+  const video4 = $('#video4').get(0);
+
+  const $play5 = $('#play-btn5');
+  const $pause5 = $('#pause-btn5');
+  const video5 = $('#video5').get(0);
+
+  const $play6 = $('#play-btn6');
+  const $pause6 = $('#pause-btn6');
+  const video6 = $('#video6').get(0);
+
+  const $asideContainer = $('.js-aside-container');
+  const $aside = $('.aside-tutorial');
+
+  const $tutOffset = $('.tutorials').position().top;
+  const $genInfoOffset = $('.general-info').position().top;
+  const $videoLessonsOffset = $('.video-lessons').position().top;
+  const $reviewsOffset = $('.reviews').position().top;
+
+  $(window).on('scroll', () => {
+    if ($(window).scrollTop() > $asideContainer.position().top) {
+      $aside.addClass('aside--fixed');
+    } else {
+      $aside.removeClass('aside--fixed');
+    }
+    if ($(window).scrollTop() > $tutOffset && $(window).scrollTop() < $genInfoOffset) {
+      $('.aside__item').removeClass('aside__item--active');
+      $(".aside__item[data-name='tutorial']").addClass('aside__item--active');
+    } else if ($(window).scrollTop() > $genInfoOffset && $(window).scrollTop() < $videoLessonsOffset) {
+      $('.aside__item').removeClass('aside__item--active');
+      $(".aside__item[data-name='general']").addClass('aside__item--active');
+    } else if ($(window).scrollTop() > $videoLessonsOffset && $(window).scrollTop() < $reviewsOffset) {
+      $('.aside__item').removeClass('aside__item--active');
+      $(".aside__item[data-name='video']").addClass('aside__item--active');
+    } else if ($(window).scrollTop() > $reviewsOffset) {
+      $('.aside__item').removeClass('aside__item--active');
+      $(".aside__item[data-name='reviews']").addClass('aside__item--active');
+    } else if ($(window).scrollTop() > $tutOffset) {
+      $('.aside__item').removeClass('aside__item--active');
+    }
+  })
+
+  // const getOffset = (block, item) => {
+  //   if ($(window).scrollTop() > block) {
+  //     item.removeClass('aside__item--active')
+  //     item.classList.add('aside__item--active');
+  //   }
+  // }
 
   if ($item.length) {
     $item.on('click', (e) => {
@@ -9,14 +68,33 @@ $(document).ready(() => {
       e.currentTarget.classList.add('aside__item--active')
     })
   }
-  $btn1.on('click', function() {
-    if (video1.paused) {
-      video1.play();
-      $btn1.addClass('hide');
-    } else {
-      video1.pause();
-      $btn1.removeClass('hide');
+  const PlayVideo = (play, video, pause) => {
+    play.on('click', function() {
+      if (video.paused) {
+        video.play();
+        play.addClass('hide');
+        pause.removeClass('hide');
+      } else {
+        video.pause();
+      }
+    })
+    if (pause) {
+      pause.on('click', () => {
+        if (video.played) {
+          video.pause();
+          play.removeClass('hide');
+          pause.addClass('hide');
+        } else {
+          video.play()
+        }
+      })
     }
-  })
+  }
+  PlayVideo($play1, video1, $pause1);
+  PlayVideo($play2, video2, $pause2);
+  PlayVideo($play3, video3, $pause3);
+  PlayVideo($play4, video4, $pause4);
+  PlayVideo($play5, video5, $pause5);
+  PlayVideo($play6, video6, $pause6);
 
 });
